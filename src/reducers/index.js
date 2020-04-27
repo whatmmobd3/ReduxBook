9144b1b474669386c2c4f26043597ca5499dbed1
quangloc.nguyen <quangloc.nguyen>
2020-04-27 14:28:32 +0700

    implementaion container

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BookReducer from './reducer_book'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books:BookReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title:'Harry'},
+        {title:'The Dark Tower'},
+        {title:'Eloquent Ruby'},
+        {title:'ffffff'}
+    ]
+}
