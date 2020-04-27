944ce429a700b90d3fc35c20221f764a922216f3
quangloc.nguyen <quangloc.nguyen>
2020-04-27 17:19:51 +0700

    consuming actions

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,34 +1,38 @@
 import React from "react";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from "redux";
 
- class BookList extends React.Component {
+class BookList extends React.Component {
   renderList() {
     return this.props.books.map((book) => {
-        return(
-            <li key={book.title} className="list-group-item">
-                {book.title}
-            </li>
-        )
+      return (
+        <li
+          key={book.title}
+          className="list-group-item"
+          onClick={() => this.props.selectBook(book)}
+        >
+          {book.title}
+        </li>
+      );
     });
   }
-  componentDidMount(){
-      console.log(this.props);
-      
+  componentDidMount() {
+    console.log(this.props);
   }
   render() {
-      return(
-          <ul className="list-group col-sm-4">
-              {this.renderList()}
-          </ul>
-      )
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
 
-
 function mapStateToProps(state) {
-    return {
-        books:state.books
-    }
+  return {
+    books: state.books,
+  };
 }
 
-export default connect(mapStateToProps)(BookList)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
